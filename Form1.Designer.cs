3f3ff6c96c0583f4376db9d94ee6fe641d6eea41
DavidBetteridge <davidabetteridge@gmail.com>
2017-03-22 08:40:40 +0000

    Initial

---
+++ Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿namespace SqlToExcel
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSQL = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtFilename = new System.Windows.Forms.TextBox();
+            this.cmdBrowse = new System.Windows.Forms.Button();
+            this.cmdCancel = new System.Windows.Forms.Button();
+            this.cmdOK = new System.Windows.Forms.Button();
+            this.cmdCS = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox1.Controls.Add(this.cmdCS);
+            this.groupBox1.Controls.Add(this.txtSQL);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtConnectionString);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(911, 344);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Data Source";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database Connection:";
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtConnectionString.Location = new System.Drawing.Point(23, 70);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(800, 25);
+            this.txtConnectionString.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(20, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "SQL Query:";
+            // 
+            // txtSQL
+            // 
+            this.txtSQL.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtSQL.Location = new System.Drawing.Point(21, 151);
+            this.txtSQL.Multiline = true;
+            this.txtSQL.Name = "txtSQL";
+            this.txtSQL.Size = new System.Drawing.Size(869, 170);
+            this.txtSQL.TabIndex = 3;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.groupBox2.Controls.Add(this.cmdBrowse);
+            this.groupBox2.Controls.Add(this.txtFilename);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(12, 377);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(913, 116);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Target";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(105, 17);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "File To Generate:";
+            // 
+            // txtFilename
+            // 
+            this.txtFilename.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilename.Location = new System.Drawing.Point(21, 62);
+            this.txtFilename.Name = "txtFilename";
+            this.txtFilename.Size = new System.Drawing.Size(751, 25);
+            this.txtFilename.TabIndex = 2;
+            // 
+            // cmdBrowse
+            // 
+            this.cmdBrowse.Location = new System.Drawing.Point(778, 62);
+            this.cmdBrowse.Name = "cmdBrowse";
+            this.cmdBrowse.Size = new System.Drawing.Size(112, 25);
+            this.cmdBrowse.TabIndex = 3;
+            this.cmdBrowse.Text = "Browse";
+            this.cmdBrowse.UseVisualStyleBackColor = true;
+            this.cmdBrowse.Click += new System.EventHandler(this.cmdBrowse_Click);
+            // 
+            // cmdCancel
+            // 
+            this.cmdCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cmdCancel.Location = new System.Drawing.Point(779, 513);
+            this.cmdCancel.Name = "cmdCancel";
+            this.cmdCancel.Size = new System.Drawing.Size(146, 42);
+            this.cmdCancel.TabIndex = 2;
+            this.cmdCancel.Text = "Cancel";
+            this.cmdCancel.UseVisualStyleBackColor = true;
+            this.cmdCancel.Click += new System.EventHandler(this.cmdCancel_Click);
+            // 
+            // cmdOK
+            // 
+            this.cmdOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdOK.Location = new System.Drawing.Point(617, 513);
+            this.cmdOK.Name = "cmdOK";
+            this.cmdOK.Size = new System.Drawing.Size(146, 42);
+            this.cmdOK.TabIndex = 3;
+            this.cmdOK.Text = "OK";
+            this.cmdOK.UseVisualStyleBackColor = true;
+            this.cmdOK.Click += new System.EventHandler(this.cmdOK_Click);
+            // 
+            // cmdCS
+            // 
+            this.cmdCS.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmdCS.Location = new System.Drawing.Point(829, 70);
+            this.cmdCS.Name = "cmdCS";
+            this.cmdCS.Size = new System.Drawing.Size(61, 28);
+            this.cmdCS.TabIndex = 4;
+            this.cmdCS.Text = "...";
+            this.cmdCS.UseVisualStyleBackColor = true;
+            this.cmdCS.Click += new System.EventHandler(this.cmdCS_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.DefaultExt = "xlsx";
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "Excel|*.xlsx|All Files|*.*";
+            this.openFileDialog1.Title = "File To Generate";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.cmdOK;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.cmdCancel;
+            this.ClientSize = new System.Drawing.Size(937, 572);
+            this.Controls.Add(this.cmdOK);
+            this.Controls.Add(this.cmdCancel);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SQL to Excel";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSQL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button cmdBrowse;
+        private System.Windows.Forms.TextBox txtFilename;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button cmdCancel;
+        private System.Windows.Forms.Button cmdOK;
+        private System.Windows.Forms.Button cmdCS;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,144 @@
+﻿using ClosedXML.Excel;
+using Microsoft.Data.ConnectionUI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SqlToExcel
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            this.txtConnectionString.Text = "Data Source=.;Initial Catalog=master;Integrated Security=True";
+            this.txtFilename.Text = @"c:\temp\extract.xlsx";
+            this.txtSQL.Text = "SELECT * FROM sys.databases";
+        }
+
+        /// <summary>
+        /// The user has clicked cancel
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmdCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmdCS_Click(object sender, EventArgs e)
+        {
+            var dataConnectionDialog = new DataConnectionDialog();
+            DataSource.AddStandardDataSources(dataConnectionDialog);
+
+            if (DataConnectionDialog.Show(dataConnectionDialog) == DialogResult.OK)
+            {
+                this.txtConnectionString.Text = dataConnectionDialog.ConnectionString;
+            }
+
+        }
+
+        private async void cmdOK_Click(object sender, EventArgs e)
+        {
+            var cs = this.txtConnectionString.Text.Trim();
+            if (string.IsNullOrWhiteSpace(cs))
+            {
+                MessageBox.Show("Please enter the connection string", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtConnectionString.Focus();
+                return;
+            }
+
+            var sql = this.txtSQL.Text.Trim();
+            if (string.IsNullOrWhiteSpace(sql))
+            {
+                MessageBox.Show("Please enter the sql query", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtSQL.Focus();
+                return;
+            }
+
+            var filename = this.txtFilename.Text.Trim();
+            if (string.IsNullOrWhiteSpace(filename))
+            {
+                MessageBox.Show("Please enter the name of the file you need to generate", this.Text, MessageBoxButtons.OK, MessageBoxIcon.Information);
+                this.txtFilename.Focus();
+                return;
+            }
+
+            try
+            {
+                using (var cn = new SqlConnection(cs))
+                {
+                    await cn.OpenAsync();
+
+                    using (var cmd = new SqlCommand())
+                    {
+                        cmd.Connection = cn;
+                        cmd.CommandType = CommandType.Text;
+                        cmd.CommandText = sql;
+
+                        using (var dr = await cmd.ExecuteReaderAsync())
+                        {
+                            var rowCount = 1;
+                            var workbook = new XLWorkbook();
+                            var worksheet = workbook.Worksheets.Add("Sheet1");
+
+                            // Get the column names
+                            for (int i = 0; i < dr.FieldCount; i++)
+                            {
+                                var name = dr.GetName(i);
+                                worksheet.Cell(GetColumnLetter(i + 1) + rowCount.ToString()).Value = name;
+                            }
+
+
+                            while (await dr.ReadAsync())
+                            {
+                                rowCount++;
+
+                                for (int i = 0; i < dr.FieldCount; i++)
+                                {
+                                    var value = dr.GetValue(i).ToString();
+                                    worksheet.Cell(GetColumnLetter(i + 1) + rowCount.ToString()).Value = value;
+                                }
+
+                            }
+
+                            workbook.SaveAs(filename);
+                            MessageBox.Show("Complete");
+                        }
+
+                    }
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), this.Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+        }
+
+        private static string GetColumnLetter(int column)
+        {
+            if (column < 1) return String.Empty;
+            return GetColumnLetter((column - 1) / 26) + (char)('A' + (column - 1) % 26);
+        }
+
+        private void cmdBrowse_Click(object sender, EventArgs e)
+        {
+            this.openFileDialog1.FileName = this.txtFilename.Text;
+            if (openFileDialog1.ShowDialog(this) == DialogResult.OK)
+            {
+                this.txtFilename.Text = openFileDialog1.FileName;
+            }
+        }
+    }
+}
